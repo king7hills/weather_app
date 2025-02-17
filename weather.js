eb55952f9890831afcc971bb65a7a01e4b9672f1
king7hills <haasjh1@gmail.com>
2025-02-17 14:20:15 -0500

    Initialized weather app. created files, simple structure.

---
+++ weather.js
@@ -0,0 +1,17 @@
+// weather.js
+let location = '';
+
+// function success(position) {
+//     const crd = position.coords;
+
+//     console.log("Current position is:")
+//     console.log(`Latitude : ${crd.latitude}`);
+//     console.log(`Longitude : ${crd.longitude}`);
+// }
+
+// function error(err) {
+//     console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error);
+
